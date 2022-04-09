a763ee7d886c8138df65fa0304cd25ad6ff612e6
Samuel Janas <sneakyinsect@gmail.com>
2022-04-09 17:11:49 +0200

    made it all in one code, just need to add the functionality of finding in BST and timers

--- main.cpp
+++ main.cpp
@@ -3,36 +3,75 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+
 #include "orderedlist.h"
+#include "tree.h"
 
 using namespace std;
 
 int main(){
+    bool DL = false, BST = true;
+
     int records = 1000;
     vector<int> v(records/10);
     iota(v.begin(), v.end(), 1000000);
     random_shuffle(v.begin(), v.end());
-    //for(auto x : v) cout<<x<<" ";
 
-    ListElement* head = NULL;
-    ListElement* PutAfter;
+    //^-Random IDs for deletion
+
     
     ifstream inData;
     inData.open("C:\\Users\\samue\\Documents\\BST AND DL\\outputTest_1.txt");
     string inputName, inputSurname;
     int inputID; //temporary variables from input
+    if(DL) {
 
-    //displayList(head);
-    while(inData>>inputName>>inputSurname>>inputID) {
-   S     //cout<<inputName<<" "<<inputSurname<<" "<<inputID<<"\n";
-        PutAfter = WhereToPut(head, inputID);
-        if (PutAfter == NULL) {
-            append(&head, inputName, inputSurname, inputID);
-        } else
-            insertAfter(PutAfter, inputName, inputSurname, inputID);
+        ListElement* head = NULL;
+        ListElement* temp;
+
+        //displayList(head);
+
+        while (inData >> inputName >> inputSurname >> inputID) {
+            //cout<<inputName<<" "<<inputSurname<<" "<<inputID<<"\n";
+            temp = WhereToPut(head, inputID);
+            if (temp == NULL) {
+                append(&head, inputName, inputSurname, inputID);
+            } else
+                insertAfter(temp, inputName, inputSurname, inputID);
+
+        }
+        displayList(head);
+    }
+
+    if(BST){
+        TreeNode* root = NULL;
+        //TreeNode* Ttemp;
+
+        //if(DL){
+            inData.clear();
+            inData.seekg(0);
+        //}
+        while (inData >> inputName >> inputSurname >> inputID) {
+            //cout<<inputName<<" "<<inputSurname<<" "<<inputID<<"\n";
+            root = insert(root, inputName, inputSurname, inputID);
+        }
+        root = deleteNode(root, 1000096);
+        cout<<"\ninorder: \n";
+        inorder(root);
+        cout<<"\npostorder: \n";
+        postorder(root);
+        cout<<"\npreorder: \n";
+        preorder(root);
+
+        root = buildTree(root);
+        cout<<"\nBalanced inorder: \n";
+        inorder(root);
+        cout<<"\nBalanced postorder: \n";
+        postorder(root);
+        cout<<"\nBalanced preorder: \n";
+        preorder(root);
 
     }
-    displayList(head);
 
     return 0;
 }
---
+++ tree.h
@@ -0,0 +1,136 @@
+#ifndef TREE_H
+#define TREE_H
+
+using namespace std;
+
+struct TreeNode{
+    string name, surname;
+    int ID;
+    struct TreeNode *left, *right;
+};
+
+TreeNode *newNode(string name, string surname, int ID) {
+    struct TreeNode* temp = new TreeNode;
+    temp->name = name;
+    temp->surname = surname;
+    temp->ID = ID;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+TreeNode *insert(TreeNode *root, string name, string surname, int ID){
+    if(root == NULL) return newNode(name, surname, ID);
+
+    if(ID < root->ID)
+        root->left = insert(root->left, name, surname, ID);
+    else
+        root->right = insert(root->right, name, surname, ID);
+
+    return root;
+}
+
+TreeNode *minValueNode(TreeNode *node) {
+    TreeNode *current = node;
+
+    // Find the leftmost leaf
+    while (current && current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+TreeNode *deleteNode(TreeNode *root, int ID){
+    if(root == NULL) return root;
+
+    if(ID < root->ID)
+        root->left = deleteNode(root->left, ID);
+    else if (ID > root->ID)
+        root->right = deleteNode(root->right, ID);
+    else {
+        // If the node is with only one child or no child
+        if (root->left == NULL) {
+            struct TreeNode *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct TreeNode *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // If the node has two children
+        struct TreeNode *temp = minValueNode(root->right);
+
+        // Place the inorder successor in position of the node to be deleted
+        root->ID = temp->ID;
+
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->ID);
+    }
+    return root;
+}
+
+void inorder(TreeNode *root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->ID << "->";
+        inorder(root->right);
+    }
+}
+
+void preorder(TreeNode *root) {
+    if (root != NULL) {
+        cout<<root->ID<<"->";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(TreeNode *root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->ID<<"->";
+    }
+}
+//The following function are here to balance an unbalanced BST
+
+TreeNode* buildTreeUtil(vector<TreeNode*> &nodes, int start, int end){
+    if (start > end)
+        return NULL;
+
+    int mid = (start + end)/2;
+    TreeNode *root = nodes[mid];
+
+    root->left  = buildTreeUtil(nodes, start, mid-1);
+    root->right = buildTreeUtil(nodes, mid+1, end);
+
+    return root;
+}
+
+void storeBSTNodes(TreeNode* root, vector<TreeNode *> &nodes)
+{
+    // Base case
+    if (root==NULL)
+        return;
+
+    // Store nodes in Inorder (which is sorted
+    // order for BST)
+    storeBSTNodes(root->left, nodes);
+    nodes.push_back(root);
+    storeBSTNodes(root->right, nodes);
+}
+
+TreeNode* buildTree(TreeNode* root)
+{
+    // Store nodes of given BST in sorted order
+    vector<TreeNode *> nodes;
+    storeBSTNodes(root, nodes);
+
+    // Constructs BST from nodes[]
+    int n = nodes.size();
+    return buildTreeUtil(nodes, 0, n-1);
+}
+
+
+#endif //TREE_H
